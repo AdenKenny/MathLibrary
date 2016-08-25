230f91224c1d4de40e40904f60f5d4e9a30ad778
Aden <adenkenny@gmail.com>
2016-08-26 10:38:22 +1200

    math

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import arith.BasicMath;
+
 public class Main {
 
-	
-	
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		
+		BasicMath b = new BasicMath();
+		
+		Class<?> c = b.getClass();
+		
+		Class<?>[] cArgs = new Class[2];
+		cArgs[0] = int.class;
+		cArgs[1] = int.class;
 
+		Method m = c.getDeclaredMethod("intAdd", cArgs);
+		m.setAccessible(true);
+		
+		int val = (int) m.invoke(b, 13, 10);
+		System.out.println(val);
 	}
 	
-
-
 }
